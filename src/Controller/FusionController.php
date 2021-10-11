52762dcb6b48e8d5dff49d0688f1e8753137cd4a
LaxhP <laxhan.push@gmail.com>
2021-10-11 17:12:40 +0200

    third commit

--- src/Controller/FusionController.php
+++ src/Controller/FusionController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use League\Csv\Reader;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -8,7 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FusionController extends AbstractController
 {
-    #[Route('/fusion', name: 'fusion')]
+
+    /**
+     * @Route ("/fusion", name= "fusion")
+     */
     public function index(): Response
     {
         return $this->json([
@@ -16,4 +20,21 @@ class FusionController extends AbstractController
             'path' => 'src/Controller/FusionController.php',
         ]);
     }
+
+    /**
+     * @Route ("/readcsv")
+     */
+    public function read(): Response
+    {
+        $csv = Reader::createFromPath('C:\Users\laxha\OneDrive\Bureau\Cours\web_projets\vetux-line\csvFile\french-data.csv', 'r');
+        $csv->setHeaderOffset(0);
+        $header = $csv->getHeader(); //returns the CSV header record
+        $records = $csv->getRecords(); //returns all the CSV records as an Iterator object
+
+
+        return $this->render('/fusion/read.html.twig', array(
+            'records' => $records,'header'=>$header
+        ));
+    }
 }
+
