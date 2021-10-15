3d732913c4a4c0ec8e10df569b2100313e87fbf1
LaxhP <laxhan.push@gmail.com>
2021-10-15 16:51:23 +0200

    good_upload

--- src/Controller/FusionController.php
+++ src/Controller/FusionController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+use App\Service\FileUploader;
 use ErrorException;
 use League\Csv\Exception;
 use League\Csv\Reader;
@@ -8,11 +9,13 @@ use League\Csv\Writer;
 
 use SplFileObject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 class FusionController extends AbstractController
 {
+
+
 
     /**
      * @Route ("/fusion", name= "fusion")
@@ -26,12 +29,21 @@ class FusionController extends AbstractController
     }
 
     /**
-     * @Route ("/readcsv")
+     * @Route ("/readcsv", name="read")
      * @throws Exception
      */
-    public function read()
+    public function read(Request $request, string $uploadDir,
+                      FileUploader $uploader)
     {
-        $csv = Reader::createFromPath('C:\Users\laxha\OneDrive\Bureau\Cours\web_projets\vetux-line\csvFile\french-data.csv', 'r');
+        $file='../var/uploads/file1.csv'  ;
+        $file2='../var/uploads/file2.csv';
+
+        if (empty($file))
+        {
+            return new Response("No file specified",  Response::HTTP_UNPROCESSABLE_ENTITY,
+                ['content-type' => 'text/plain']);
+        }
+        $csv = Reader::createFromPath($file, 'r');
         $csv->setHeaderOffset(0);
         $header = $csv->getHeader(); //returns the CSV header record
         $records = $csv->getRecords(); //returns all the CSV records as an Iterator object
@@ -44,8 +56,12 @@ class FusionController extends AbstractController
 
 
 
+        unlink('../var/uploads/file1.csv');
+        unlink('../var/uploads/file2.csv');
         return $this->render('/fusion/read.html.twig', array(
             'records' => $records,'header'=>$header
+
+
         ));
     }
 }
