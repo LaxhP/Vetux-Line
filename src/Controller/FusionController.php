b39db85036736e1a09d1c4d6c25b6935d27150c2
LaxhP <laxhan.push@gmail.com>
2021-10-17 21:53:17 +0200

    upload

--- src/Controller/FusionController.php
+++ src/Controller/FusionController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-use ErrorException;
+
 use League\Csv\Exception;
 use League\Csv\Reader;
 use League\Csv\Writer;
@@ -10,8 +10,6 @@ use SplFileObject;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Config\Framework\RequestConfig;
-use function Sodium\add;
 
 class FusionController extends AbstractController
 {
@@ -31,17 +29,18 @@ class FusionController extends AbstractController
 
 //Number,Gender,NameSet,Title,GivenName,MiddleInitial,Surname,StreetAddress,City,State,StateFull,ZipCode,Country,CountryFull,EmailAddress,Username,Password,BrowserUserAgent,TelephoneNumber,TelephoneCountryCode,MothersMaiden,Birthday,TropicalZodiac,CCType,CCNumber,CVV2,CCExpires,NationalID,UPS,WesternUnionMTCN,MoneyGramMTCN,Color,Occupation,Company,Vehicle,Domain,BloodType,Pounds,Kilograms,FeetInches,Centimeters,Latitude,Longitude
     /**
-     * @Route ("/readcsv")
+     * @Route ("/readcsv", name="readcsv")
      * @throws Exception
      */
     public function read()
     {
-        $source="/home/laupa/Vidéos/Vetux-Line/Vetux/Vetux-Line/csvFile/";
-        $csv = Reader::createFromPath($source . 'french-data.csv', 'r');
+        //$source="/home/laupa/Vidéos/Vetux-Line/Vetux/Vetux-Line/csvFile/";
+        $file='../var/uploads/file1.csv'  ;
+        $csv = Reader::createFromPath($file, 'r');
         $csv->setHeaderOffset(0);
         $header = $csv->getHeader(); //returns the CSV header record
         $records = $csv->getRecords(); //returns all the CSV records as an Iterator object
-        $output = Writer::createFromPath($source.'output.csv');
+        $output = Writer::createFromPath('../public/csv/output.csv');
         //$tabName = ["Gender", "GivenName","Surname","Birthday","StreetAddress","Title","EmailAddress","TelephoneNumber","Kilograms","CCType","CCNumber","CVV2","CCExpires","Vehicle"];
           $tabName = ["Gender", "GivenName","Surname","Birthday","StreetAddress","EmailAddress","Centimeters","FeetInches"];
         $output->insertOne($tabName);//0,
---
+++ src/Controller/UploadController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\FileUploader;
+use Psr\Log\LoggerInterface;
+
+
+class UploadController extends AbstractController
+{
+    /**
+     * @Route("/upload")
+     */
+    public function index(): Response
+    {
+        return $this->render('upload/index.html.twig', [
+            'controller_name' => 'UploadController',
+        ]);
+    }
+
+    /**
+     * @Route("/doUpload", name="do-upload")
+     * @param Request $request
+     * @param string $uploadDir
+     * @param FileUploader $uploader
+     * @param LoggerInterface $logger
+     * @return Response
+     */
+    public function upload(Request $request, string $uploadDir,
+                          FileUploader $uploader, LoggerInterface $logger): Response
+    {
+        $token = $request->get("token");
+
+        if (!$this->isCsrfTokenValid('upload', $token))
+        {
+            $logger->info("CSRF failure");
+
+            return new Response("Operation not allowed",  Response::HTTP_BAD_REQUEST,
+                ['content-type' => 'text/plain']);
+        }
+
+        $file1 = $request->files->get('file1');
+        $file2 = $request->files->get('file2');
+
+        if (empty($file1))
+        {
+            return new Response("No file specified",
+                Response::HTTP_UNPROCESSABLE_ENTITY, ['content-type' => 'text/plain']);
+        }
+
+        if (empty($file2))
+        {
+            return new Response("No file2 specified",
+                Response::HTTP_UNPROCESSABLE_ENTITY, ['content-type' => 'text/plain']);
+        }
+
+        $filename = 'file1.csv';
+        $uploader->upload($uploadDir, $file1, $filename);
+
+        $filename = 'file2.csv';
+        $uploader->upload($uploadDir, $file2, $filename);
+
+       /* return new Response("File uploaded",  Response::HTTP_OK,
+            ['content-type' => 'text/plain']);
+       */
+        return $this->redirectToRoute('readcsv');
+    }
+}
